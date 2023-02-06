98e5dbba649751aa76c979a74fbc3c9561075277
Rafael Papa <rafaelpapa81@gmail.com>
2023-02-05 19:11:34 -0500

    add some elements

--- App.js
+++ App.js
@@ -1,20 +1,33 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, StyleSheet, Text, TouchableOpacity, View, Image, SafeAreaViewBase, Button } from 'react-native';
 
 // IOs: this view will be mapped to UIView
 // Android: View -> AndroidView
 export default function App() {
+  const handlePress = () => console.log("Button tapped");
+
   return (
-    <View style={styles.container}>
+    <SafeAreaView style={styles.container}>
       <Text>Hello Raf!</Text>
+      <TouchableOpacity onPress={() => console.log("Image tapped")}>
+        <Image 
+          source={{
+            width: 200,
+            height: 300, 
+            uri: "https://picsum.photos/200/300",
+            }} 
+        />
+      </TouchableOpacity>
+      <Button title='Click Me' onPress={handlePress}/>
+      
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    flex: 1, //the entirely screen
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
