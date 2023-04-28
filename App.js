0228d4776ebcb53877471acb8e05ddb9c0d3ac0e
Rafael Papa <rafaelpapa81@gmail.com>
2023-04-28 19:09:13 -0400

    align position relative

--- App.js
+++ App.js
@@ -15,16 +15,12 @@ export default function App() {
         flexDirection: "row", // horizontal axis is te main
         justifyContent: "center", // center obj at main axis - it depends of flexDirection. Row is horizontal = x
         alignItems: "center", // align items to secondary axis. Row is vertical = y
-        alignContent: "center", // alignment of the entire content, not only elements of each line - only works with Wrap
-        flexWrap: "wrap",
       }}
     >
       <View 
         style={{
           backgroundColor: "dodgerblue",
-          flexBasis: 100, // width or height 
-          flexGrow: 1, // same as positive flex, flex: 1
-          //width: 100,
+          width: 100,
           height: 100,
         }}
       />
@@ -32,7 +28,8 @@ export default function App() {
         style={{
           backgroundColor: "gold",
           width: 100,
-          height: 100,
+          height: 100,  // in Reactive Native position relative is default for all components
+          top: -20, // same as bottom: 20 (relative to its original position, do not affect the layout around)
         }}
       />
       <View 
