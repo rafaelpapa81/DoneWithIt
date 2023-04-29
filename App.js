fcccc26f2b7a18f23ee5181b39137f5af62f27e5
Rafael Papa <rafaelpapa81@gmail.com>
2023-04-29 17:10:16 -0400

    app: first screen and organization

--- App.js
+++ App.js
@@ -2,46 +2,11 @@ import { StatusBar } from 'expo-status-bar';
 import {
   View, 
 } from 'react-native';
+import WelcomeScreen from './app/screens/WelcomeScreen';
 
 
 // IOs: this view will be mapped to UIView
 // Android: View -> AndroidView
 export default function App() {
-  return (
-    <View 
-      style={{
-        backgroundColor: "#fff",
-        flex: 1,
-        flexDirection: "row", // horizontal axis is te main
-        justifyContent: "center", // center obj at main axis - it depends of flexDirection. Row is horizontal = x
-        alignItems: "center", // align items to secondary axis. Row is vertical = y
-      }}
-    >
-      <View 
-        style={{
-          backgroundColor: "dodgerblue",
-          width: 100,
-          height: 100,
-        }}
-      />
-      <View 
-        style={{
-          backgroundColor: "gold",
-          width: 100,
-          height: 100,  // in Reactive Native position relative is default for all components
-          top: 20,
-          left: 20,
-          position: "absolute",
-        }}
-      />
-      <View 
-        style={{
-          backgroundColor: "tomato",
-          width: 100,
-          height: 100,
-        }}
-      />
-      <StatusBar style="auto" />
-    </View>
-  );
+  return <WelcomeScreen/>;
 }
